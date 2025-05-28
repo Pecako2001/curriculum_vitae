04367e34d6c510c4dc4b62dcb5015e42b3c3c3dc
Koen van Wijlick <koenvanwijlick@gmail.com>
2025-05-28 10:15:19 +0200

    update on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
   return (
     <html lang="en" {...mantineHtmlProps}>
       <head>
