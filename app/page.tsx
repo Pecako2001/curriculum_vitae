4774384952a280aca422787d9790bb73ddd25025
Koen van Wijlick <koenvanwijlick@gmail.com>
2025-05-28 11:36:31 +0200

    Add About component import to HomePage and adjust Navbar styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Introduction from "@/components/Introduction/Introduction";
 import ModernTimeline from "@/components/CV/ModernTimeline/ModernTimeline";
+import About from "@/components/About/About";
 import SoftwareExperience from "@/components/SoftwareExperience/SoftwareExperience";
 import Contact from "@/components/Homepage/Contact";
 import Skills from "@/components/Skills/Skills";
