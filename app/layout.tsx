5a8e9f0d4d6bcc7be0e0f73330ad8da138a52f0e
Koen van Wijlick <77498283+Pecako2001@users.noreply.github.com>
2025-05-28 14:09:43 +0200

    Add viewport meta tag for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Curriculum Vitae</title>
         <link rel="icon" href="/avatar.png" />
       </head>
